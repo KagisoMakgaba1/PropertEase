7f4df558f4c99067316a0e0774dc8db0ade345c0
Kagiso Makgaba <kagisomakgaba@outlook.com>
2024-10-18 15:59:39 +0200

    Complete update listing functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -93,7 +93,7 @@ export default function Profile() {
       dispatch(updateUserFailure(error.message));
     }
   };
-  
+
   const handleDeleteUser = async () => {
     try {
       dispatch(deleteUserStart());
@@ -250,7 +250,9 @@ export default function Profile() {
 
       {userListings && userListings.length > 0 && (
         <div className="flex flex-col gap-4">
-          <h1 className="text-center mt-7 text-2xl font-semibold">Your Listings</h1>
+          <h1 className="text-center mt-7 text-2xl font-semibold">
+            Your Listings
+          </h1>
           {userListings.map((listing) => (
             <div
               key={listing._id}
@@ -270,8 +272,15 @@ export default function Profile() {
                 <p>{listing.name}</p>
               </Link>
               <div className="flex flex-col item-center">
-                <button onClick={() => handleListingDelete(listing._id)} className="text-red-700 uppercase">Delete</button>
-                <button className="text-green-700 uppercase">Edit</button>
+                <button
+                  onClick={() => handleListingDelete(listing._id)}
+                  className="text-red-700 uppercase"
+                >
+                  Delete
+                </button>
+                <Link to={`/update-listing/${listing._id}`}>
+                  <button className="text-green-700 uppercase">Edit</button>
+                </Link>
               </div>
             </div>
           ))}{" "}
