0118257b8ea72a141500b263c6d87495bafb819c
Kagiso Makgaba <kagisomakgaba@outlook.com>
2024-10-18 14:37:27 +0200

    Complete show user listings functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -19,10 +19,10 @@ import {
   signOutUserStart,
 } from "../redux/user/userSlice";
 import { useDispatch } from "react-redux";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 export default function Profile() {
   const fileRef = useRef(null);
-  const {currentUser, loading, error} = useSelector((state) => state.user);
+  const { currentUser, loading, error } = useSelector((state) => state.user);
   const [file, setFile] = useState(undefined);
   const [filePerc, setFilePerc] = useState(0);
   const [fileUploadError, setFileUploadError] = useState(false);
@@ -122,7 +122,7 @@ export default function Profile() {
     } catch (error) {
       dispatch(deleteUserFailure(error.message));
     }
-  }
+  };
   const handleShowListings = async () => {
     try {
       setShowListingsError(false);
@@ -135,9 +135,8 @@ export default function Profile() {
       setUserListings(data);
     } catch (error) {
       setShowListingsError(true);
-
     }
-  }
+  };
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
@@ -197,8 +196,12 @@ export default function Profile() {
         >
           {loading ? "loading..." : "update"}
         </button>
-        <Link className="bg-gree-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95" to={'/create-listing'}>
-        Create Listing</Link>
+        <Link
+          className="bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95"
+          to={"/create-listing"}
+        >
+          Create Listing
+        </Link>
       </form>
       <div className="flex justify-between mt-5">
         <span
@@ -207,31 +210,50 @@ export default function Profile() {
         >
           Delete account
         </span>
-        <span onClick={handleSignOut} className="text-red-700 cursor-pointer">Sign out</span>
-
+        <span onClick={handleSignOut} className="text-red-700 cursor-pointer">
+          Sign out
+        </span>
       </div>
       <p className="text-red-700 mt-5">{error ? error : ""}</p>
       <p className="text-green-700 mt-5">
         {updateSuccess ? "User is updated successfully!" : ""}
       </p>
-      <button onClick={handleShowListings} className='text-green-700 w-full'>Show Listings</button>
+      <button onClick={handleShowListings} className="text-green-700 w-full">
+        Show Listings
+      </button>
       <p className="text-red-700 mt-5">
         {showListingsError ? "Error showing listings" : ""}
       </p>
 
-      {userListings && userListings.length > 0 &&
-      userListings.map((listing) => ( <div key={listing._id} className='border rounded-lg p-3 flex justify-between items-center gap-4'>
-          <link to={`/listing/${listing._id}`}>
-            <img src={listing.imageUrls[0]} alt='listing cover'className='h-16 w-16 object-contain'/>
-          </link>
-          <link className='text-slate-700 font-semibold hover:underline truncate flex-1' to={`/listing/${listing._id}`}>
-            <p>{listing.name}</p>
-          </link>
-          <div className='flex flex-col item-center'>
-            <button className="text-red-700 uppercase">Delete</button>
-            <button className="text-green-700 uppercase">Edit</button>
-          </div>
-        </div>))}
+      {userListings && userListings.length > 0 && (
+        <div className="flex flex-col gap-4">
+          <h1 className="text-center mt-7 text-2xl font-semibold">Your Listings</h1>
+          {userListings.map((listing) => (
+            <div
+              key={listing._id}
+              className="border rounded-lg p-3 flex justify-between items-center gap-4"
+            >
+              <Link to={`/listing/${listing._id}`}>
+                <img
+                  src={listing.imageUrls[0]}
+                  alt="listing cover"
+                  className="h-16 w-16 object-contain"
+                />
+              </Link>
+              <Link
+                className="text-slate-700 font-semibold hover:underline truncate flex-1"
+                to={`/listing/${listing._id}`}
+              >
+                <p>{listing.name}</p>
+              </Link>
+              <div className="flex flex-col item-center">
+                <button className="text-red-700 uppercase">Delete</button>
+                <button className="text-green-700 uppercase">Edit</button>
+              </div>
+            </div>
+          ))}{" "}
+        </div>
+      )}
     </div>
   );
 }
