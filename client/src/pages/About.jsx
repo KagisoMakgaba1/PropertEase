2a8caa31d05839f3e25598c898a38145af31653a
rabs23 <ruthrabs22@gmail.com>
2024-10-27 04:10:07 +0100

    About page completed

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,8 +2,26 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>
-      About
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>About PropertEase</h1>
+      <p className='mb-4 text-slate-700'>PropertEase is a real estate agency that specializes in
+        helping clients buy, sell and rent properties in great and
+        convenient locations. We have a team of experienced real estate
+        agents who are dedicated to providing the best service to our clients.
+      </p>
+      <p className='mb-4 text-slate-700'>
+        Our goal is to make the process of buying, selling or renting a property
+        as easy and stress-free as possible. We understand that buying or selling
+        a property can be a daunting task, which is why we are here to help you
+        every step of the way.
+      </p>
+      <p className='mb-4 text-slate-700'>
+        Whether you are looking to buy your dream home, sell your current property,
+        or rent a property, we are here to help. Our team of experienced real estate
+        agents will work with you to find the perfect property that meets your needs
+        and budget. We will guide you through the entire process, from start to finish,
+        to ensure that you have a smooth and successful experience.
+      </p>
     </div>
   )
 }
