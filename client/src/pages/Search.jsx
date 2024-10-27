37ff9e68a73814ac831d2921ca7efeadad5399da
rabs23 <ruthrabs22@gmail.com>
2024-10-27 03:49:01 +0100

    complete home page

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -2,7 +2,6 @@ import { set } from 'mongoose';
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import ListingItem from '../components/ListingItem';
-//import e from 'express';
 
 export default function Search() {
     const navigate = useNavigate()
